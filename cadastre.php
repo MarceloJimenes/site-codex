11e860403833619778c088d9fc7b6d1bcccbf128
Marcelo Jimenes <marcelojimenes1506@gmail.com>
2021-04-05 18:52:55 -0300

    update

--- cadastre.php
+++ cadastre.php
@@ -1,15 +1,119 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Codex - Cadastro</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- links -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+
+    <link rel="stylesheet" href="style.scss">
+	
+
+	<title>Cadastro de Usuário</title>
 </head>
 <body>
-    <h3>Bem vindo a página de cadastro!</h3>
-    <form>
-        <p>Estrutura do Formulario</p>
-    </form>
+	<main>
+		<section class="hero is-fullheight is-hero-background">
+
+			<!-- HERO HEAD -->
+			<div class="hero-head">
+				<header class="navbar">
+					<div class="container">
+						<div class="navbar-brand">
+							<a class="navbar-item" href="../../index.php">
+									<p class="title logo-title-menu">
+										CODEX
+									</p>
+								</a>
+							<!-- Menu responsivo -->
+							<span class="navbar-burger" data-target="navbarMenuHeroC">
+								<span></span>
+								<span></span>
+								<span></span>
+							</span>
+						</div>
+
+						<!-- Default menu -->
+						<div id="navbarMenuHeroC" class="navbar-menu">
+							<div class="navbar-end">
+								<a class="navbar-item" href="../../index.php">
+									Página inicial
+								</a>
+								<!-- botao para download -->
+								<span class="navbar-item">
+                                    <a class="button is-success is-inverted">
+                                        <span class="icon">
+                                            <i class="fab fa-github"></i>
+                                        </span>
+                                        <span>Download</span>
+                                    </a>
+                                </span>
+							</div>
+						</div>
+					</div>
+				</header>
+			</div>
+
+			<!-- HERO CONTENT -->
+			<div class="hero-body">
+
+				<div class="container">
+                <link rel="stylesheet" href="../../styles/backgrounds.scss">
+
+					<form class="form" action="../../scripts/php/register/cadastrar_usuario.php" method="post">
+
+						<h1 class="label has-text-centered">Cadastro</h1>
+
+                        <figure>
+                            <!-- aqui vai a logo do codex -->
+                            <img src="" alt="">
+                        </figure>
+						
+						<div>
+							<label class="label">Nome Completo: <span>*</span> </label>
+							<input class="input" type="text" name="nome" required/>
+						</div>
+
+                        <div>
+							<label class="label">Usuário: <span>*</span></label>
+							<input class="input" type="text" name="usuario" required/>
+						</div>
+
+                        <div>
+							<label class="label">Senha: <span>*</span></label>
+							<input class="input" type="password" name="senha" required/>
+						</div>
+
+                        <div>
+							<label class="label">Confirmação de senha: <span>*</span></label>
+							<input class="input" type="password" name="confirma_senha" required/>
+						</div>
+
+
+						<div>
+							<label class="label">E-mail: <span>*</span></label>
+							<input class="input" type="text" name="email" required/>
+						</div>
+
+
+						<div class="botoes has-text-centered">
+							<input class="button is-success" type="submit" value="Cadastrar"/>
+						</div>
+						
+					</form>
+				
+				</div>
+			</div>
+
+		</section>
+	</main>
+	<?php
+	
+		
+	
+	?>
 </body>
 </html>
+
