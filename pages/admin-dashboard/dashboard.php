f0767f57a38054362f76ea2e4a950219c68827aa
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2021-04-30 01:34:26 -0300

    add and update admin-dashboard page

--- pages/admin-dashboard/dashboard.php
+++ pages/admin-dashboard/dashboard.php
@@ -6,18 +6,37 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
-
-  <style>
-    @import url("../../theme/global.css");
-
-
-  </style>
+  <link rel="stylesheet" href="dashboard.css">
 
 </head>
 <body>
   <section>
     <div class="menu">
+      <h1 class="logo"><span>CODEX</span> Admin</h1>
+
+      <div class="linksBox">
+        <p class="topics">Cadastrar</p>
+        <p class="links"><a href="#">Cadastrar Linguagens</p></a>
+        <p class="links"><a href="#">Cadastrar Aulas</p></a>
+        <p class="links"><a href="#">Cadastrar Exercícios</p></a>
+        <p class="links"><a href="#">Cadastrar Aulas</p></a>
+      </div>
+
+      <div class="linksBox">
+        <p class="topics">Alterar</p>
+        <p class="links"><a href="#">Alterar Linguagens</p></a>
+        <p class="links"><a href="#">Alterar Aulas</p></a>
+        <p class="links"><a href="#">Alterar Exercícios</p></a>
+        <p class="links"><a href="#">Alterar Aulas</p></a>
+      </div>
+
+      <div class="linksBox">
+        <p class="topics">Remover</p>
+        <p class="links"><a href="#">Remover Linguagens</p></a>
+        <p class="links"><a href="#">Remover Aulas</p></a>
+        <p class="links"><a href="#">Remover Exercícios</p></a>
+        <p class="links"><a href="#">Remover Aulas</p></a>
+      </div>
 
     </div>
     <div class="content">
