8bf50449b21f92e3ab4479da768270637c88da7b
Carlos Henrique Ramos Cardoso <carloshenriqueramoscardoso@gmail.com>
2021-05-05 18:39:32 -0300

    add content list

--- pages/admin-dashboard/views/classUpdate.php
+++ pages/admin-dashboard/views/classUpdate.php
@@ -1,2 +1,41 @@
-<h1>Atualizar alguma Aula</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam corporis quam quia. Nulla</p>
+<link rel="stylesheet" href="styles/table.css">
+<div class="box-content">
+  <h1 class="title has-text-centered">Aulas Cadastradas</h1>
+
+  <table class="table">
+    <tr>
+      <td>Aula</td>
+      <td>Descrição Aula</td>
+      <td>Conteudo</td>
+      <td>Descrição Conteúdo</td>
+      <td>excluir</td>
+      <td>modificar</td>
+    </tr>
+    <?php
+      $sql = $conn -> query ("select * from conteudo");
+
+      while($i = $sql -> fetch_array()) {
+        echo "
+          <tr>
+            <td>$i[tt_aula]</td>
+            <td>$i[ds_aula]</td>
+            <td>$i[tt_conteudo]</td>
+            <td>$i[ds_conteudo]</td>
+            <td>
+              <a href='#'>
+                <img src='../../assets/svgs/delete.svg' class='trash'/>
+              </a>
+            </td>
+            <td>
+              <a href='#'>
+                <img src='../../assets/svgs/edit.svg' class='edit'/>
+              </a>
+            </td>
+          </tr>
+        ";
+      }
+    
+    ?>
+  </table>
+
+</div>
--- pages/admin-dashboard/views/exerUpdate.php
+++ pages/admin-dashboard/views/exerUpdate.php
@@ -1,2 +1,49 @@
-<h1>Atualizar algum Exercício</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam corporis quam quia. Nulla</p>
+<link rel="stylesheet" href="styles/table.css">
+<div class="box-content">
+  <h1 class="title has-text-centered">Exercícios Cadastrados</h1>
+
+  <table class="table">
+    <tr>
+      <td>Linguagem</td>
+      <td>Aula</td>
+      <td>Exercício</td>
+      <td>Enunciado</td>
+      <td>excluir</td>
+      <td>modificar</td>
+    </tr>
+    <?php
+      $sql = $conn -> query 
+      ("
+        select * from exercicios 
+        inner join 
+        conteudo on exercicios.id_conteudo = conteudo.id_conteudo
+        inner join
+        linguagens on conteudo.id_linguagem = linguagens.id_linguagem
+      ");
+
+      while($i = $sql -> fetch_array()) {
+
+        echo "
+          <tr>
+            <td>$i[nome]</td>
+            <td>$i[tt_aula]</td>
+            <td>$i[tt_exercicio]</td>
+            <td>$i[ds_exercicio]</td>
+            <td>
+              <a href='../../scripts/php/productDelete/index.php?id=$i[id_produto]'>
+                <img src='../../assets/svgs/delete.svg' class='trash'/>
+              </a>
+            </td>
+            <td>
+              <a href='../../scripts/php/updateProduct/index.php?id=$i[id_produto]'>
+                <img src='../../assets/svgs/edit.svg' class='edit'/>
+              </a>
+            </td>
+          </tr>
+        ";
+      }
+    
+    ?>
+  </table>
+
+</div>
