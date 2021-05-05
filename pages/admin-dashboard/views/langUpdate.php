7095f6bb1dc9aa23714447294f2f29b93c530d2f
Carlos Henrique Ramos Cardoso <carloshenriqueramoscardoso@gmail.com>
2021-05-05 18:02:50 -0300

    update adm dashboard

--- pages/admin-dashboard/views/langUpdate.php
+++ pages/admin-dashboard/views/langUpdate.php
@@ -1,2 +1,39 @@
-<h1>Atualizar alguma Linguagem</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam corporis quam quia. Nulla</p>
+<link rel="stylesheet" href="styles/table.css">
+<div class="box-content">
+  <h1 class="title has-text-centered">Linguagens Cadastradas</h1>
+
+  <table class="table">
+    <tr>
+      <td>Linguagem</td>
+      <td>Descrição</td>
+      <td>Tipo</td>
+      <td>excluir</td>
+      <td>modificar</td>
+    </tr>
+    <?php
+      $sql = $conn -> query ("select * from linguagens");
+
+      while($i = $sql -> fetch_array()) {
+        echo "
+          <tr>
+            <td>$i[nome]</td>
+            <td>$i[descricao]</td>
+            <td>$i[tipo]</td>
+            <td>
+              <a href='../../scripts/php/productDelete/index.php?id=$i[id_produto]'>
+                <img src='../../assets/svgs/delete.svg' class='trash'/>
+              </a>
+            </td>
+            <td>
+              <a href='../../scripts/php/updateProduct/index.php?id=$i[id_produto]'>
+                <img src='../../assets/svgs/edit.svg' class='edit'/>
+              </a>
+            </td>
+          </tr>
+        ";
+      }
+    
+    ?>
+  </table>
+
+</div>
