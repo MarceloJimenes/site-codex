a4f1d32f2722da9c6fc6e3a93817f26487151725
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2021-05-05 19:49:23 -0300

    test on analytics

--- pages/admin-dashboard/views/statistics.php
+++ pages/admin-dashboard/views/statistics.php
@@ -2,12 +2,15 @@
 <link rel="stylesheet" href="styles/statistics.css">
 
 <!-- Iniciando conteúdo -->
-<h1 id="teste">Sugestive Title</h1>
-<hr>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita nam quidem est impedit, vitae consectetur cupiditate ut, ex consequatur, nostrum officia assumenda molestiae mollitia possimus atque quas totam repudiandae! A.</p>
-<ul>
-  <li>teste 1</li>
-  <li>teste 2</li>
-  <li>teste 3</li>
-  <li>teste 4</li>
-</ul>
+<div class="box">
+  <h1 class="title">Sugestive Title</h1>
+  <hr>
+  <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita nam quidem est impedit, vitae consectetur cupiditate ut, ex consequatur, nostrum officia assumenda molestiae mollitia possimus atque quas totam repudiandae! A.</p>
+  <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita nam quidem est impedit, vitae consectetur cupiditate ut, ex consequatur, nostrum officia assumenda molestiae mollitia possimus atque quas totam repudiandae! A.</p>
+  <ul class="any">
+    <li>teste 1</li>
+    <li>teste 2</li>
+    <li>teste 3</li>
+    <li>teste 4</li>
+  </ul>
+</div>
